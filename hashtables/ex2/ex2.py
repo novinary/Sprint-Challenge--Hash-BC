f86b2608014a09586721571b460ddc0cb413c938
novinary <novina.dev@gmail.com>
2019-07-12 10:38:22 +0100

    Comleted implementation for reconstruct_trip

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -16,8 +16,24 @@ def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
     route = [None] * length
 
-    """
-    YOUR CODE HERE
-    """
+    your_ticket = None
+
+    # Add key, value to the has table for every ticket 
+    for ticket in range(length):
+        hash_table_insert(hashtable, tickets[ticket].source, tickets[ticket].destination)
+        # Check if the ticket flight is None
+        if tickets[ticket].source == "NONE":
+            # Assign it to your_ticket
+            your_ticket = tickets[ticket].destination
+            # Assign your_ticket to first route array
+            route[0] = your_ticket
+
+    for ticket in range(1, length):
+        # Use hash_table_retrieve to find next destination
+        next_destination = hash_table_retrieve(hashtable, your_ticket)
+        # Assign next_destination to the route array
+        route[ticket] = next_destination
+        # Assing your_ticket to next destination
+        your_ticket = next_destination
 
     return route
